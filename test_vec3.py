5fce7c53e5cf4baece02766755dcd5f220e887e5
Michael Shaw <michael.gene.shaw@gmail.com>
2020-01-20 15:41:48 -0600

    added #normalize and #add

--- test_vec3.py
+++ test_vec3.py
@@ -2,7 +2,14 @@ import math
 import vec3
 
 def create_vector(values):
-        return vec3.Vect(values['components'])
+    return vec3.Vect(values['components'])
+
+def print_test(description='', test_name='', expected=0, actual=0):
+    print("Results of {} {}:".format(description, test_name))
+    if expected == actual:
+        print("Pass")
+    else:
+        print("Expected {} and got {}".format(expected, actual))
     
 
 class TestVect():
@@ -12,17 +19,33 @@ class TestVect():
     def test_magnitude(self):
         for k, v in self.component_dict.iteritems():
             vec = create_vector(v)
-            print("Results of {} Magnitude: {}".format(v['description'], vec.magnitude() == v['magnitude']))
+            print_test(v['description'], 'Magnitude', v['magnitude'], vec.magnitude())
             
+        return True
+
+    def test_normalize(self):
+        for k, v in self.component_dict.iteritems():
+            vec = create_vector(v)
+            print_test(v['description'], 'Normalize', vec3.Vect(v['normal']).components, vec.normalize().components)
+
         return True
 
     def test_dot_product(self):
         vec1 = create_vector(self.component_dict[0])
         vec2 = create_vector(self.component_dict[1])
         answer = -27
-        
-        print("Results of {} and {} Dot Product: {}".format(vec1.components, vec2.components, vec1.dot_product(vec2) == answer))
-        print("Results of {} and {} Dot Product: {}".format(vec2.components, vec1.components, vec2.dot_product(vec1) == answer))
+
+        print_test("v1 and v2", 'Dot Product', answer, vec1.dot_product(vec2))
+        print_test('v2 and v1', 'Dot Product', answer, vec2.dot_product(vec1))
+
+        return True
+
+    def test_add(self):
+        vec1 = create_vector(self.component_dict[0])
+        vec2 = create_vector(self.component_dict[1])
+        answer = [4, 4, 7]
+
+        print_test("v1 and v2", "add", answer, (vec1 + vec2).components)
 
         return True
     
@@ -30,14 +53,18 @@ class TestVect():
 if __name__ == '__main__':
     test_data = {0: {'description': 'basic 3d vector with negative elements',
                      'components': [1, -2, -2],
-                     'magnitude': 3
+                     'magnitude': 3,
+                     'normal': [1/3., -2/3., -2/3.]
                      },
                  1: {'description': 'basic 3d vector',
                      'components': [3, 6, 9],
-                     'magnitude': math.sqrt(81+45)
+                     'magnitude': math.sqrt(81+45),
+                     'normal': [3 / math.sqrt(81+45), 6 / math.sqrt(81+45), 9 / math.sqrt(81+45)]
                      }
                  }
     test = TestVect(test_data)
     test.test_magnitude()
     test.test_dot_product()
+    test.test_normalize()
+    test.test_add()
     
--- vec3.py
+++ vec3.py
@@ -14,10 +14,25 @@ Components to be passed as an array [c1, c2, c3, ..., cn] of numerics."""
 
         return math.sqrt(radicand)
 
+    def normalize(self):
+        normal_v_l = []
+        magnitude = self.magnitude()
+        for c in self.components:
+            normal_v_l.append(c / magnitude)
+
+        return Vect(normal_v_l)
+
     def dot_product(self, vec2):
         sum = 0
         for i, c in enumerate(self.components):
             sum += (c * vec2.components[i])
 
         return sum
+
+    def __add__(self, vec2):
+        out_components = []
+        for i, c1 in enumerate(self.components):
+            out_components.append(c1 + vec2.components[i])
+
+        return Vect(out_components)
     
