8e61ad85bc01d64c15bf77b1363bb3367ead5b8c
Michael Shaw <michael.gene.shaw@gmail.com>
2020-01-22 19:50:14 -0600

    Finished vec3 conversion

--- vec3.py
+++ vec3.py
@@ -8,49 +8,26 @@ class Vec3():
         self.z = z
 
     def magnitude(self):
-        radicand = (self.x ** 2) + (self.y ** 2) + (self.z ** 2)
-
-        return math.sqrt(radicand)
+        return math.sqrt(self.dot_product(self))
 
     def normalize(self):
-        normal_v_l = []
-        magnitude = self.magnitude()
-        for c in self.components:
-            normal_v_l.append(c / magnitude)
+        mag = self.magnitude()
 
-        return Vect(normal_v_l)
+        return Vec3(self.x/mag, self.y/mag, self.z/mag)
 
     def dot_product(self, vec2):
-        sum = 0
-        for i, c in enumerate(self.components):
-            sum += (c * vec2.components[i])
+        dp = (self.x * vec2.x) + (self.y * vec2.y) + (self.z * vec2.z)
 
-        return sum
+        return dp
 
     def __add__(self, vec2):
-        out_components = []
-        for i, c1 in enumerate(self.components):
-            out_components.append(c1 + vec2.components[i])
-
-        return Vect(out_components)
+        return Vec3(self.x + vec2.x, self.y + vec2.y, self.z + vec2.z)
     
     def __sub__(self, vec2):
-        out_components = []
-        for i, c1 in enumerate(self.components):
-            out_components.append(c1 - vec2.components[i])
-
-        return Vect(out_components)
+        return Vec3(self.x - vec2.x, self.y - vec2.y, self.z - vec2.z)
 
     def __mul__(self, scalar):
-        out_components = []
-        for c in self.components:
-            out_components.append(c * scalar)
-
-        return Vect(out_components)
+        return Vec3(self.x * scalar, self.y * scalar, self.z * scalar)
 
     def __truediv__(self, divisor):
-        out_components = []
-        for c in self.components:
-            out_components.append(c / divisor)
-
-        return Vect(out_components)
+        return Vec3(self.x / divisor, self.y / divisor, self.z / divisor)
