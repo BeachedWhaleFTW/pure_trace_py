568c217400d98446096cd065fb157ff7b788fdc8
Michael Shaw <michael.gene.shaw@gmail.com>
2020-01-24 18:01:38 -0600

    added Image

---
+++ image.py
@@ -0,0 +1,7 @@
+class Image:
+    def __init__(self, args):
+        self.size = args["size"]
+        self.max_color_val = args["max_color_val"]
+
+    def save_to_ppm(self, filename):
+        pass
