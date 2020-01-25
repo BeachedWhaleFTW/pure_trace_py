0bae04b0617245dfdf16f9509b145f92683f49bb
Michael Shaw <michael.gene.shaw@gmail.com>
2020-01-24 18:33:52 -0600

    added color_data attr and #save_to_ppm

--- image.py
+++ image.py
@@ -1,7 +1,26 @@
+import os
+
+
+
 class Image:
     def __init__(self, args):
         self.size = args["size"]
         self.max_color_val = args["max_color_val"]
+        self.color_data = args["color_data"]
 
     def save_to_ppm(self, filename):
-        pass
+        if os.path.exists(filename):
+            os.unlink(filename)
+
+        with open(filename, 'w') as f:
+            f.write("P3 {} {}\n".format(self.size[0], self.size[1]))
+            f.write("{}\n".format(self.max_color_val))
+            for r in self.color_data:
+                for i, c in enumerate(r):
+                    if (i < self.size[0] - 1):
+                        f.write("{} {} {} ".format(c[0], c[1], c[2]))
+                    else:
+                        f.write("{} {} {}".format(c[0], c[1], c[2]))
+                f.write("\n")
+
+        print("file saved as {}".format(filename))
