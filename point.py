a380007749c37eafedeabc245b20f0a07765ddeb
Michael Shaw <michael.gene.shaw@gmail.com>
2020-01-28 20:43:27 -0600

    made point an alias of vec3

--- point.py
+++ point.py
@@ -1,14 +1,6 @@
-class Point:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+from vec3 import Vec3
 
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self, other):
-        return Point(self.x - other.x, self.y - other.y, self.z - other.z)
-
-    def __pow__(self, power, modulo=None):
-        return Point(self.x ** power, self.y ** power, self.z ** power)
+class Point(Vec3):
+    """Point stores the coords. Alias for Vector"""
+    pass
